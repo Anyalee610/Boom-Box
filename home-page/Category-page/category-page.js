a7958523ae16f7ad0dc4784b32fa581dfabae94e
“Anyalee610” <anayalambright610@gmail.com>
2022-09-26 01:14:27 -0400

    clean code on category

--- home-page/Category-page/category-page.js
+++ home-page/Category-page/category-page.js
@@ -1,111 +1,112 @@
 //Dom Elements 
-const form = document.getElementById('search');
-const button = document.getElementById('button');
-const foodpic = document.getElementById('foodpic');
-const foodInstructions = document.getElementById('foodInstructions');
-const ingredient = document.getElementById('ingredient');
-const ingredientTiltle = document.getElementById('ingredient-title');
-const countries = document.getElementById('countries');
-const countriesfood = document.getElementById('countriesfood');
-const body = document.querySelector('body')
-const itemDisplay = document.getElementById('display-item')
-const innerDisplay = document.getElementById('inner-display')
-const recipeDiv = document.getElementById('countries')
-countries.style.display='flex'
-itemDisplay.style.display= 'none'
+const form = document.getElementById("search");
+const button = document.getElementById("button");
+const foodpic = document.getElementById("foodpic");
+const foodInstructions = document.getElementById("foodInstructions");
+const ingredient = document.getElementById("ingredient");
+const ingredientTiltle = document.getElementById("ingredient-title");
+const countries = document.getElementById("countries");
+const countriesfood = document.getElementById("countriesfood");
+const body = document.querySelector("body");
+const itemDisplay = document.getElementById("display-item");
+const innerDisplay = document.getElementById("inner-display");
+const recipeDiv = document.getElementById("countries");
+
+//first actions is to show the countries 
+countries.style.display = "flex";
+itemDisplay.style.display = "none";
 
 //function for when the button is clicked on 
 const searchForRecipes = (event) =>{
     event.preventDefault()
-    countries.style.display ='none'
+    countries.style.display = "none";
     let name = form.value;
     if (name.length === 1){
         itemDisplay.style.display = "none";
-        countriesfood.innerHTML = ''
+        countriesfood.innerHTML = "";
         countriesfood.style.display = "flex";
         fetch(`https://www.themealdb.com/api/json/v2/9973533/search.php?f=${name}`)
         .then(res=> res.json())
         .then(json => json.meals.forEach(meal => {
-            let div = document.createElement('div');
-            div.setAttribute('class', 'card');
-            div.setAttribute('style','width: 18rem;');
-            let img = document.createElement('img');
-            img.setAttribute('class', 'card-img-top');
-            let innerDiv = document.createElement('div');
-            innerDiv.setAttribute('class','card-body')
-            let p = document.createElement('p');
-            p.setAttribute('class','card-text');
+            let div = document.createElement("div");
+            div.setAttribute("class", "card");
+            div.setAttribute("style","width: 18rem;");
+            let img = document.createElement("img");
+            img.setAttribute("class", "card-img-top");
+            let innerDiv = document.createElement("div");
+            innerDiv.setAttribute("class","card-body")
+            let p = document.createElement("p");
+            p.setAttribute("class","card-text");
             innerDiv.append(p);
-            div.append(img,innerDiv)
+            div.append(img,innerDiv);
             const mealName = meal.strMeal;
             const mealPic = meal.strMealThumb;
             const mealId = meal.idMeal;
-            div.setAttribute('id',`${mealId}`)
-            img.setAttribute('src', `${mealPic}`)
-            img.setAttribute('id',`${mealId}`)
-            p.setAttribute('id',`${mealId}`)
-            p.innerText = mealName
-            countriesfood.append(div)
+            div.setAttribute("id",`${mealId}`);
+            img.setAttribute("src", `${mealPic}`);
+            img.setAttribute("id",`${mealId}`);
+            p.setAttribute("id",`${mealId}`);
+            p.innerText = mealName;
+            countriesfood.append(div);
             
         
-    }))
-    button.addEventListener('click',searchForRecipes)
+            }))
+        //if the click again it will start again andd check what they put in the form
+        button.addEventListener("click",searchForRecipes);
         
     
     }else{fetch(`https://www.themealdb.com/api/json/v2/9973533/search.php?s=${name}`)
         .then(el=> el.json())
         .then(el => {
             itemDisplay.style.display = "flex";
-            countriesfood.style.display='none'
-            console.log(name)
-            foodpic.src = el.meals[0].strMealThumb
-            foodInstructions.innerText = el.meals[0].strInstructions
-            ingredientTiltle.innerText = "Ingredients"
-            
+            countriesfood.style.display = "none";
+            foodpic.src = el.meals[0].strMealThumb;
+            foodInstructions.innerText = el.meals[0].strInstructions;
+            ingredientTiltle.innerText = "Ingredients";
             //we reset the innerText for ingredients
             ingredient.innerText = "";
-            ingredient.appendChild(ingredientTiltle)
+            ingredient.appendChild(ingredientTiltle);
             //loop to set the foodingredients with the ingredients we fetched
             for(let i = 1; i< 30; i++){
                 if(el.meals[0][`strIngredient${i}`]){
-                    const foodingredients = document.createElement('dd')
-                    foodingredients.innerText = el.meals[0][`strIngredient${i}`]
-                    ingredient.appendChild(foodingredients)
+                    const foodingredients = document.createElement("dd");
+                    foodingredients.innerText = el.meals[0][`strIngredient${i}`];
+                    ingredient.appendChild(foodingredients);
                 }
             }
             
 
         })
-        button.addEventListener('click',searchForRecipes)
+        button.addEventListener("click",searchForRecipes);
     
 }
 }
+button.addEventListener("click",searchForRecipes);
 
-button.addEventListener('click',searchForRecipes)
 
 const countriesClickHandler = (event) => {
     let countryName = event.target.textContent.trim();
-    console.log(countryName)
-    countries.style.display='none'
+    countries.style.display = "none";
+
     fetch(`https://www.themealdb.com/api/json/v2/9973533/filter.php?a=${countryName}`)
     .then(el=> el.json())
     .then(meal => meal.meals.forEach(meal => {
-        let div = document.createElement('div');
-        div.setAttribute('class', 'card');
-        div.setAttribute('style','width: 18rem;');
-        let img = document.createElement('img');
-        img.setAttribute('class', 'card-img-top');
-        let innerDiv = document.createElement('div');
-        innerDiv.setAttribute('class','card-body')
+        let div = document.createElement("div");
+        div.setAttribute("class", "card");
+        div.setAttribute("style","width: 18rem;");
+        let img = document.createElement("img");
+        img.setAttribute("class", "card-img-top");
+        let innerDiv = document.createElement("div");
+        innerDiv.setAttribute("class","card-body")
         let p = document.createElement('p');
-        p.setAttribute('class','card-text');
+        p.setAttribute("class","card-text");
         innerDiv.append(p);
         div.append(img,innerDiv);
         const mealName = meal.strMeal;
         const mealPic = meal.strMealThumb;
         const mealId = meal.idMeal;
-        div.setAttribute('id',`${mealId}`);
-        img.setAttribute('src', `${mealPic}`);
+        div.setAttribute("id",`${mealId}`);
+        img.setAttribute("src", `${mealPic}`);
         p.innerText = mealName;
         countriesfood.append(div);
 
@@ -118,17 +119,17 @@ const countriesClickHandler = (event) => {
 
 
 countriesfood.addEventListener('click', (e) => {
-    let nameId = e.target.id
-    itemDisplay.style.display = 'flex';
-    innerDisplay.style.display = 'block';
-    countriesfood.style.display = 'none';
+    let nameId = e.target.id;
+    itemDisplay.style.display = "flex";
+    innerDisplay.style.display = "block";
+    countriesfood.style.display = "none";
     fetch(`https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=${nameId}`)
     .then(el=>el.json())
     .then(el => {
-        // itemDisplay.style.display='inline'
-        foodpic.src = el.meals[0].strMealThumb
-        foodInstructions.innerText = el.meals[0].strInstructions
-      
+        foodpic.src = el.meals[0].strMealThumb;
+        foodInstructions.innerText = el.meals[0].strInstructions;
+    
+
         //we reset the innerText for ingredients
         ingredient.innerText = "";
         ingredientTiltle.innerText = "Ingredients"
@@ -136,7 +137,7 @@ countriesfood.addEventListener('click', (e) => {
         //loop to set the foodingredients with the ingredients we fetched
         for(let i = 1; i< 21; i++){
             if(el.meals[0][`strIngredient${i}`]){
-                const foodingredients = document.createElement('dd')
+                const foodingredients = document.createElement("dd")
                 foodingredients.innerText = el.meals[0][`strIngredient${i}`]
                 ingredient.appendChild(foodingredients)
             }
@@ -178,26 +179,26 @@ const data = [
 {img:"tn", text: "Tunisian"},
 {img:"tr", text: "Turkish"},
 {img:"us", text: "American"},
-{img:"vn", text: "Vietnamese"},]
+{img:"vn", text: "Vietnamese"},];
 
 const insertFlags = (arrayOfFlags) => {
 for (let i = 0; i < arrayOfFlags.length; i++){
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('pic-country')
-    let imgData = data[i].img
-    let imgText = data[i].text
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("pic-country");
+    let imgData = data[i].img;
+    let imgText = data[i].text;
     let template = `<img  class = 'foodsimgs' src="/Users/anaya-lambright/Development/unit-6/ESCA-FOODS/image/${imgData}.png">
     <p class = 'foodstext' >${imgText}</p>
-    `
-    newDiv.innerHTML = template
+    `;
+    newDiv.innerHTML = template;
     
     countries.append(newDiv);
     
 }
 }
-insertFlags(data)
+insertFlags(data);
 
-countries.addEventListener('click',countriesClickHandler)
+countries.addEventListener("click",countriesClickHandler);
 
 
 
