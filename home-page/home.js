8964c8bd818332d38c890097429d3ff5130af4fd
“Anyalee610” <anayalambright610@gmail.com>
2022-09-25 22:38:39 -0400

    added latest meals

--- home-page/home.js
+++ home-page/home.js
@@ -1,27 +1,62 @@
 //Dom Elements 
-const form = document.getElementById('search')
+const form = document.getElementById('search');
 const button = document.getElementById('button');
 const foodpic = document.getElementById('foodpic');
-const foodInstructions = document.getElementById('foodInstructions')
-const ingredient = document.getElementById('ingredient')
-const ingredientTiltle = document.getElementById('ingredient-title')
-const oneRecipe = document.getElementById('one-recipe')
-const mealinfo = document.getElementsByClassName('nav-item meal-type')
-const homesearch = document.getElementById('home-search')
-const recipeDiv = document.getElementById('recipes')
-const itemDisplay = document.getElementById('display-item')
-const innerDisplay = document.getElementById('inner-display')
-//function for when the button is clicked on 
+const foodInstructions = document.getElementById('foodInstructions');
+const ingredient = document.getElementById('ingredient');
+const ingredientTiltle = document.getElementById('ingredient-title');
+const oneRecipe = document.getElementById('one-recipe');
+const mealinfo = document.getElementsByClassName('nav-item meal-type');
+const homesearch = document.getElementById('home-search');
+const recipeDiv = document.getElementById('recipes');
+const itemDisplay = document.getElementById('display-item');
+const innerDisplay = document.getElementById('inner-display');
+const title = document.getElementById('title');
+
 itemDisplay.style.display = "none";
-recipeDiv.style.display='none'
+
+fetch(`https://www.themealdb.com/api/json/v2/9973533/latest.php`)
+        .then(res=> res.json())
+        .then(json => json.meals.forEach(meal => {
+            let div = document.createElement('div');
+            div.setAttribute('class', 'card');
+            div.setAttribute('style','width: 18rem;');
+            let img = document.createElement('img');
+            img.setAttribute('class', 'card-img-top');
+            let innerDiv = document.createElement('div');
+            innerDiv.setAttribute('class','card-body')
+            let p = document.createElement('p');
+            p.setAttribute('class','card-text');
+            innerDiv.append(p);
+            div.append(img,innerDiv)
+            const mealName = meal.strMeal;
+            const mealPic = meal.strMealThumb;
+            const mealId = meal.idMeal;
+            div.setAttribute('id',`${mealId}`)
+            img.setAttribute('src', `${mealPic}`)
+            img.setAttribute('id',`${mealId}`)
+            p.setAttribute('id',`${mealId}`)
+            p.innerText = mealName
+            recipeDiv.append(div)
+            
+        
+    }))
+
+
+
+//function for when the button is clicked on 
  const searchForRecipes = (event) =>{
+    itemDisplay.style.display = "none";
+    title.style.display = "none"
+    recipeDiv.innerHTML= ""
     event.preventDefault()
+
     let name = form.value;
     if (name.length === 1){
         itemDisplay.style.display = "none";
         recipeDiv.innerHTML = ''
         recipeDiv.style.display = "flex";
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${name}`)
+        fetch(`https://www.themealdb.com/api/json/v2/9973533/search.php?f=${name}`)
         .then(res=> res.json())
         .then(json => json.meals.forEach(meal => {
             let div = document.createElement('div');
@@ -53,7 +88,7 @@ recipeDiv.style.display='none'
     }else{
         itemDisplay.style.display = "flex";
         recipeDiv.style.display='none'
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
+        fetch(`https://www.themealdb.com/api/json/v2/9973533/search.php?s=${name}`)
         .then(el=> el.json())
         .then(el => {
             console.log(name)
@@ -85,7 +120,7 @@ recipeDiv.addEventListener('click', (e) => {
     innerDisplay.style.display = 'block';
     recipeDiv.style.display = 'none';
     let nameId = e.target.id
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${nameId}`)
+    fetch(`https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=${nameId}`)
     .then(el=>el.json())
     .then(el => {
         // itemDisplay.style.display='inline'
